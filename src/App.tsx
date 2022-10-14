fa0811f3954a9575edb455aa00840f0817d7b831
ederjbbb <ederjb@icloud.com>
2022-10-14 17:45:23 +0100

    tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ export function App() {
   
 
   return (
-    <  >
-          <h1 className="font-bold text 2xl text-violet-500" >Starting</h1>
-          
-
+    <>
+          <h1 className="font-bold text-5xl text-cyan-500" >Starting</h1>
+          <button className='bg-cyan-300 font-medium px-4 py-2 rounded text-white 
+          hover:bg-violet-700 ' >Enviar</button>
     </>
   )
 }
